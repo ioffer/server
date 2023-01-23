65cffc36a94a16b372996767a409559ed8721e3a
qasim raheem 🚀 <qasimmehmood13936@gmail.com>
2023-01-23 17:17:13 +0500

    sending email bug fixed

--- sendemail.js
+++ sendemail.js
@@ -1,12 +1,5 @@
-
 const nodemailer = require('nodemailer');
 const { google } = require('googleapis');
-//accesstoken = ya29.a0AX9GBdWilJuBtrplPbhDEXCnlK2sWQia3msZ7tnvUhHnvqnZyDFS5frnaW1BqMNx2JgnLSlqcOBjhvcF7xJ0zpuxVaigaSu6U-BM9K1QFpD2CqllmF8P-V8ppqmjrX5YiinBOaYR1WuXfkSd35U5mHed-F_5aCgYKAY4SARASFQHUCsbCpadc3RfumlFSp86sYZcx8A0163
-// These id's and secrets should come from .env file.
-const CLIENT_ID = '29505227254-tbac2dam9vscrkdv3c3feh0ugbjn08mr.apps.googleusercontent.com';
-const CLEINT_SECRET = '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';
-const REDIRECT_URI = 'https://developers.google.com/oauthplayground';
-const REFRESH_TOKEN = '1//04SJYEJJSFt6-CgYIARAAGAQSNwF-L9IrYXGs3EtcDKL2Yq4OKjmgPdeII8gZRe7LvDh7gISNqPxnbXKLK3KJRxmp-7LiREChLyM';
 
 const oAuth2Client = new google.auth.OAuth2(
     CLIENT_ID,
